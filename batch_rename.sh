fcc226edb774ffca8347451f8ba2f24ed4ac15cd
Patrick Martin <pkmartin82@gmail.com>
2017-12-31 13:23:14 -0500

    Updating batch_rename.sh to better handle input as well as formatting.

--- batch_rename.sh
+++ batch_rename.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 function showUsage {
-    echo "Usage:  " `basename ${0}`" -s <suffix> -ext <extension>"
+    echo "Usage:  " `basename ${0}`" -s <suffix> -e <extension>"
 }
 
 if [ $# -eq 0 ]; 
@@ -10,14 +10,14 @@ then
     exit 1
 fi
 
-while getopts ":s:ext:" o; do
+while getopts ":s:e:" o; do
     case "${o}" in
         s)
             echo "got into s case"
-	    suffix = ${OPTARG}
+	    suffix=${OPTARG}
             ;;
-        ext)
-            echo "got into ext case"
+        e)
+            echo "got into e case"
 	    ext=${OPTARG}
             ;;
         *)
@@ -26,11 +26,16 @@ while getopts ":s:ext:" o; do
             ;;
     esac
 done
-shift $((OPTIND-1))
+shift ${OPTIND-1}
 
+echo "EXT = ${ext}"
+echo "SUFFIX = ${suffix}"
 
 # Preview command
-for FILE in *.PNG; do echo mv "${FILE}" "${FILE/.PNG/${suffix}.PNG}"; done
+for FILE in *.${ext} 
+do 
+    echo mv "${FILE}" "${FILE/.${ext}/${suffix}.${ext}}"
+done
 
 # Actual command
 #for FILE in *.PNG; do mv "${FILE}" "${FILE/.PNG/_p.PNG}"; done
