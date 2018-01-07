dd6a343d72d27822be83e7f389b2f431e601ad7d
Patrick Martin <pkmartin82@gmail.com>
2018-01-07 12:03:54 -0500

    Fixed parameter handling; added verbosity, preview; streamlined logic

--- batch_rename.sh
+++ batch_rename.sh
@@ -1,43 +1,62 @@
 #!/bin/sh
 
 function showUsage {
-    echo "Usage:  " `basename ${0}`" -s <suffix> -e <extension>"
+    echo "Usage:  " `basename ${0}`" -p -s <suffix> -e <extension>"
 }
 
-if [ $# -eq 0 ]; 
+if [ $# -lt 2 ]; 
 then
     showUsage
     exit 1
 fi
 
-while getopts ":s:e:" o; do
+while getopts "pvs:e:" o; do
     case "${o}" in
+	p)
+	    previewOnly=true
+	    ;;
         s)
-            echo "got into s case"
 	    suffix=${OPTARG}
             ;;
         e)
-            echo "got into e case"
 	    ext=${OPTARG}
             ;;
+	v) 
+	    verbose=true
+	    ;;
         *)
-            echo "got into * case"
 	    showUsage
             ;;
     esac
 done
 shift ${OPTIND-1}
 
-echo "EXT = ${ext}"
-echo "SUFFIX = ${suffix}"
+# Print Variables
+if [[ "${verbose}" = "true" ]]
+then
+	echo "VERBOSE = ${verbose}"
+	echo "PREVIEW ONLY = ${previewOnly}"
+	echo "EXT = ${ext}"
+	echo "SUFFIX = ${suffix}"
+fi
 
-# Preview command
-for FILE in *.${ext} 
+# Do Stuff!
+
+
+for FILE in `ls *.${ext}`
 do 
-    echo mv "${FILE}" "${FILE/.${ext}/${suffix}.${ext}}"
+	NEWFILE="${FILE/.${ext}/${suffix}.${ext}}"
+
+	if [[ "${previewOnly}" = "true" ]]
+	then
+		# Preview command
+		echo "Preview:    mv ${FILE} ${NEWFILE}" 
+	else		
+		if [[ "${verbose}" = "true" ]]
+		then
+			echo "Moving ${FILE} to ${NEWFILE}"
+		fi
+		mv "${FILE}" "${NEWFILE}"
+	fi
 done
-
-# Actual command
-#for FILE in *.PNG; do mv "${FILE}" "${FILE/.PNG/_p.PNG}"; done
-
 
